39b099d06c35f3c3db6cb69ead6110ba305076bd
Tyler Peckenpaugh <tyler@ty-pe.com>
2018-06-05 14:50:19 -0400

    added htaccess

--- src/App.js
+++ src/App.js
@@ -1,65 +1,66 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Banner from "./_Banner.js";
-import { Switch, Route, withRouter } from "react-router-dom";
+import {Switch, Route, withRouter} from "react-router-dom";
 import Page from "./Page";
 import data from "./_data";
-import { GoEye, GoMarkGithub } from "react-icons/lib/go";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import {GoEye, GoMarkGithub} from "react-icons/lib/go";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 class App extends Component {
   render() {
-    const { match, location, history } = this.props;
-    const routes = Object.keys(data).map(section => {
-      const intro = <p>{data[section].intro}</p> || "";
-      const subsections =
-        typeof data[section].items !== "undefined"
-          ? data[section].items.map(subsection => {
-              const image = subsection.ss ? (
-                <img
+    const {match, location, history} = this.props;
+    const routes = Object
+      .keys(data)
+      .map(section => {
+        const intro = <p>{data[section].intro}</p> || "";
+        const subsections = typeof data[section].items !== "undefined"
+          ? data[section]
+            .items
+            .map(subsection => {
+              const image = subsection.ss
+                ? (<img
                   className="card-img-top"
-                  src={
-                    subsection.ss.indexOf("http") >= 0
-                      ? subsection.ss
-                      : "http://ty-pe.com/images/screenshots/" + subsection.ss
-                  }
-                  alt={"Screenshot of " + subsection.title + "."}
-                />
-              ) : (
-                ""
-              );
+                  src={subsection
+                  .ss
+                  .indexOf("http") >= 0
+                  ? subsection.ss
+                  : "http://ty-pe.com/images/screenshots/" + subsection.ss}
+                  alt={"Screenshot of " + subsection.title + "."}/>)
+                : ("");
               const title = subsection.title
                 ? subsection.title
                 : subsection.section;
-              const repo = subsection.git ? (
-                <a href={subsection.git} className="card-link">
-                  <GoMarkGithub color="black" size=".8rem" className="mr-1" />
-                  GitHub
-                </a>
-              ) : (
-                ""
-              );
-              const link = subsection.url ? (
-                <a href={subsection.url} className="card-link">
-                  <GoEye color="black" size=".8rem" className="mr-1" />
-                  Demo
-                </a>
-              ) : (
-                ""
-              );
+              const repo = subsection.git
+                ? (
+                  <a href={subsection.git} className="card-link">
+                    <GoMarkGithub color="black" size=".8rem" className="mr-1"/>
+                    GitHub
+                  </a>
+                )
+                : ("");
+              const link = subsection.url
+                ? (
+                  <a href={subsection.url} className="card-link">
+                    <GoEye color="black" size=".8rem" className="mr-1"/>
+                    Demo
+                  </a>
+                )
+                : ("");
               let text = subsection.description
                 ? subsection.description + " "
                 : "";
-              text += subsection.status ? subsection.status : "";
+              text += subsection.status
+                ? subsection.status
+                : "";
               const techs = subsection.techs
-                ? subsection.techs.map(tech => {
-                    return <li />;
+                ? subsection
+                  .techs
+                  .map(tech => {
+                    return <li/>;
                   })
                 : "";
               return (
-                <div
-                  key={section + "-" + title}
-                  className="py-2 col-12 col-md-6"
-                >
+                <div key={section + "-" + title} className="py-2 col-12 col-md-6">
                   <div className="card shadow">
                     {image}
                     <div className="card-body subsection-body">
@@ -73,38 +74,45 @@ class App extends Component {
               );
             })
           : "";
-      return (
-        <Route
+        return (<Route
           key={section}
           path={"/" + section}
           render={() => (
-            <Page title={section.charAt(0).toUpperCase() + section.slice(1)}>
-              {" "}
-              {intro} <div className="row">{subsections} </div>{" "}
-            </Page>
-          )}
-        />
-      );
-    });
+          <Page
+            title={section
+            .charAt(0)
+            .toUpperCase() + section.slice(1)}>
+            {" "}
+            {intro}
+            <div className="row">{subsections}
+            </div>{" "}
+          </Page>
+        )}/>);
+      });
     return (
       <div className="App">
         <div className="wrap">
-          <Banner full={true} />
+          <Banner full={true}/>
           <main>
             <TransitionGroup className="routes">
               <CSSTransition
                 key={location.key}
                 classNames={{
-                  appear: "animated",
-                  appearActive: "bounceIn",
-                  enter: "animated",
-                  enterActive: "bounceIn",
-                  exit: "animated",
-                  exitActive: "fadeOutDown"
-                }}
-                timeout={400}
-              >
-                <Switch location={location}>{routes}</Switch>
+                appear: "animated",
+                appearActive: "bounceIn",
+                enter: "animated",
+                enterActive: "bounceIn",
+                exit: "animated",
+                exitActive: "fadeOutDown"
+              }}
+                timeout={400}>
+                <Switch location={location}>
+                  {routes}
+                  <Route
+                    render={() => {
+                    return <div></div>
+                  }}/>
+                </Switch>
               </CSSTransition>
             </TransitionGroup>
           </main>
--- src/_data.js
+++ src/_data.js
@@ -1,11 +1,11 @@
 import React from "react";
 export default {
-  home: {
+  home : {
     intro: (
       <div>
         <p>
-          Welcome! This site is currently a work in progress. Check back soon to
-          see how it develops.
+          Welcome! This site is currently a work in progress. Check back soon to see how
+          it develops.
         </p>
         <p>
           You can see my other site at{" "}
@@ -14,9 +14,9 @@ export default {
       </div>
     )
   },
-  designs: {
-    intro:
-      "Selected designs. These are mock-ups or demos, not live sites, unless otherwise stated.",
+  designs : {
+    intro: "Selected designs. These are mock-ups or demos, not live sites, unless otherwise " +
+        "stated.",
     items: [
       {
         title: "Underpowered",
@@ -24,15 +24,13 @@ export default {
         url: "http://peckenpaugh.us/demos/",
         status: "Defunct; community moved to facebook.",
         ss: "underpowered.png"
-      },
-      {
+      }, {
         title: "Age of Conan",
         description: "Fan site for an online game.",
         url: "http://peckenpaugh.us/demos/aoc/aoc.htm",
         status: "Defunct; client no longer interested in game.",
         ss: "aoc.png"
-      },
-      {
+      }, {
         title: "Peckenpaugh.us",
         description: "Personal portfolio site.",
         url: "http://peckenpaugh.us",
@@ -41,35 +39,35 @@ export default {
       }
     ]
   },
-  apps: {
+  apps : {
     intro: "Selected projects.",
     items: [
       {
         title: "XDM",
         description: "Team randomizer for Dice Masters tabletop game.",
         url: "http://gotu-game.com/XDM",
-        techs: ["AngularJS", "PHP"],
-        git: "https://github.com/moui72/XDM",
-        ss:
-          "http://peckenpaugh.us/wp-content/uploads/2017/11/teleplaza.gc_.cuny_.edu_.png"
-      },
-      {
+        techs: [
+          "AngularJS", "PHP"
+        ],
+        git: "https://github.com/moui72/XDM"
+      }, {
         title: "RankGames",
-        description:
-          "This app is intended to help you build ranked lists of games from those in your " +
-          "collection on Board Game Geek.",
+        description: "This app is intended to help you build ranked lists of games from those in your " +
+            "collection on Board Game Geek.",
         url: "http://rankgames.ty-pe.com/",
-        techs: ["Angular 2", "Ionic", "TypeScript"],
+        techs: [
+          "Angular 2", "Ionic", "TypeScript"
+        ],
         git: "https://github.com/moui72/ionRankGames"
-      },
-      {
+      }, {
         title: "RankGames2",
-        description:
-          "This app is intended to help you build ranked lists of games from those in your " +
-          "collection on Board Game Geek.Total rewrite of my previous RankGames app.Perform" +
-          "ance should be much improved, and I think the animations really help usability.",
+        description: "This app is intended to help you build ranked lists of games from those in your " +
+            "collection on Board Game Geek.Total rewrite of my previous RankGames app.Perform" +
+            "ance should be much improved, and I think the animations really help usability.",
         url: "http://gotu-game.com/rankgames2",
-        techs: ["Vue.js", "Vuex", "Animate.css"],
+        techs: [
+          "Vue.js", "Vuex", "Animate.css"
+        ],
         git: "https://github.com/moui72/rankgames2"
       }
     ]
@@ -93,26 +91,21 @@ let re = {
       {
         section: "strengths",
         items: [
-          "High level of expertise and experience with HTML, CSS and JavaScript",
-          "Proficient with several front-end JavaScript libraries, including JQuery and Ang" +
-            "ularJS",
+          "High level of expertise and experience with HTML, CSS and JavaScript", "Proficient with several front-end JavaScript libraries, including JQuery and Ang" +
+              "ularJS",
           "Experience with back-end coding, including PHP and Express.js/Node.js, and datab" +
-            "ases such as MySQL and SQLite",
+              "ases such as MySQL and SQLite",
           "Experience with Adobe Photoshop, Adobe InDesign, and some experience with Adobe " +
-            "Flash/ActionScript",
+              "Flash/ActionScript",
           "Experienced with git revision control",
           "Quick learner, excellent problem-solver, and independently motivated",
           "Excellent communication and interpersonal skills, especially with regard to comm" +
-            "unicating technical information to non-technically oriented listeners"
+              "unicating technical information to non-technically oriented listeners"
         ]
-      },
-      {
+      }, {
         section: "objective",
-        items: [
-          "Create elegant, functional applications and responsive websites."
-        ]
-      },
-      {
+        items: ["Create elegant, functional applications and responsive websites."]
+      }, {
         section: "work history",
         items: [
           {
@@ -120,61 +113,48 @@ let re = {
             location: "New York, NY",
             position: "Webmaster",
             date: "2014-2015",
-            duty:
-              "Created and maintained website content for the linguistics department using the " +
-              "Kentico CMS."
-          },
-          {
-            employer:
-              "Writing in the Disciplines, LaGuardia Community College, CUNY",
+            duty: "Created and maintained website content for the linguistics department using the " +
+                "Kentico CMS."
+          }, {
+            employer: "Writing in the Disciplines, LaGuardia Community College, CUNY",
             location: "Long Island City, NY",
             position: "Writing Fellow",
             date: "September 2013 - June 2014",
-            duty:
-              "Lead seminars meant to help faculty from various disciplines integrate writing i" +
-              "nto their curriculum. Designed materials for these seminars."
-          },
-          {
+            duty: "Lead seminars meant to help faculty from various disciplines integrate writing i" +
+                "nto their curriculum. Designed materials for these seminars."
+          }, {
             employer: "Second Language Acquisition Lab, CUNY Graduate Center",
             location: "New York, NY",
             position: "Research Assistant",
             date: "January 2009 - January 2011",
-            duty:
-              "Collected and entered data for a projected related to literacy and bilingualism." +
-              " Ran statistics using SPSS and analyzed results."
-          },
-          {
+            duty: "Collected and entered data for a projected related to literacy and bilingualism." +
+                " Ran statistics using SPSS and analyzed results."
+          }, {
             employer: "The Common Language Project",
             location: "Seattle, WA",
             position: "Web Developer",
             date: "January 2008 - January 2009",
-            duty:
-              "Built and maintained a content management system for an online news magazine, an" +
-              "d provided technical and design expertise."
-          },
-          {
+            duty: "Built and maintained a content management system for an online news magazine, an" +
+                "d provided technical and design expertise."
+          }, {
             employer: "ResTek, Western Washington University",
             location: "Bellingham, WA",
             position: "Webmaster and Public Relations Committee Member",
             date: "January 2004 - January 2008",
-            duty:
-              "Wrote content, designed graphics, and technically maintained a website which edu" +
-              "cated students regarding the use of the school's computer networks and online se" +
-              "rvices; wrote and designed educational materials as part of a PR campaign to imp" +
-              "rove students' technical literacy."
-          },
-          {
+            duty: "Wrote content, designed graphics, and technically maintained a website which edu" +
+                "cated students regarding the use of the school's computer networks and online se" +
+                "rvices; wrote and designed educational materials as part of a PR campaign to imp" +
+                "rove students' technical literacy."
+          }, {
             employer: "Self-employed",
             location: "Seattle, WA",
             position: "Web Designer and Developer",
             date: "January 2003 - January 2009",
-            duty:
-              "Designed websites and web applications for numerous freelance clients, and provi" +
-              "ded design and technical expertise to those clients."
+            duty: "Designed websites and web applications for numerous freelance clients, and provi" +
+                "ded design and technical expertise to those clients."
           }
         ]
-      },
-      {
+      }, {
         section: "education",
         items: [
           {
@@ -182,33 +162,19 @@ let re = {
             location: "New York, NY",
             degree: "Ph.D. in Theoretical Linguistics",
             status: "In progress, expected 2016. Current GPA: 4.0."
-          },
-          {
+          }, {
             school: "Western Washington University",
             location: "Bellingham, WA",
-            degree:
-              "Double major B.A. in Theoretical Linguistics, English Literature",
+            degree: "Double major B.A. in Theoretical Linguistics, English Literature",
             status: "Graduated cum laude, 2008"
           }
         ]
-      },
-      {
+      }, {
         section: "other skills and experience",
-        items: [
-          "Experience with LaTeX typesetting software",
-          "Experience with Wordpress, Drupal and Kentico content management systems",
-          "Extensive research and academic writing experience",
-          "Experience with SPSS and R statistical packages"
-        ]
-      },
-      {
+        items: ["Experience with LaTeX typesetting software", "Experience with Wordpress, Drupal and Kentico content management systems", "Extensive research and academic writing experience", "Experience with SPSS and R statistical packages"]
+      }, {
         section: "other interests",
-        items: [
-          "Theoretical linguistics",
-          "Accessibility for the Internet",
-          "Information structure",
-          "User interface design"
-        ]
+        items: ["Theoretical linguistics", "Accessibility for the Internet", "Information structure", "User interface design"]
       }
     ]
   }
